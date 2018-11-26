bef2d18aba03ef7d5b54d2e16697b94769c400db
coderaiser <coderaiser@cloudcmd.io>
2018-11-26 21:09:32 +0200

    feature(auto-globals) add support of activeElement

--- test/auto-globals.js
+++ test/auto-globals.js
@@ -26,6 +26,27 @@ test('auto-globals', (t) => {
     t.end();
 });
 
+test('auto-globals: document: activeElement', (t) => {
+    const tape = (str, fn) => {
+        fn(t);
+    };
+    
+    const autoTest = autoGlobals(tape);
+    const f = () => global.document.activeElement.addEventListener('click', noop);
+    
+    let called = false;
+    
+    autoTest('hello', (t, {document}) => {
+        f();
+        
+        const {activeElement} = document;
+        called = activeElement.addEventListener.calledWith('click', noop);
+    });
+    
+    t.ok(called, 'should call addEventListener');
+    t.end();
+});
+
 test('auto-globals: FormData', (t) => {
     const tape = (str, fn) => {
         fn(t);
